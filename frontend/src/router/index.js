77631a674dc2c151462cf290f1431731d3fbe776
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2025-10-14 14:41:10 -0500

    Crea archivos de componentes, vistas y assets

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+import Expediente from '@/views/Expediente.vue'
+import ExpedienteDetalle from '@/views/ExpedienteDetalle.vue'
+import SubirDocumento from '@/views/SubirDocumento.vue'
 
+const routes = [
+  { path: '/', name: 'home', component: Home },
+  { path: '/expediente', name: 'expediente', component: Expediente },
+  { path: '/expediente/:id', name: 'expediente-detalle', component: ExpedienteDetalle, props: true },
+  { path: '/subir-documento', name: 'subir-documento', component: SubirDocumento },
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [],
