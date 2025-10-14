87fa29f76a88bfc85df7ad5fc92584b679ddf912
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2025-10-14 12:13:10 -0500

    Configura archivo de vistas para los modelos

--- backend/expedientes/views.py
+++ backend/expedientes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Expediente, Documento
+from .serializers import ExpedienteSerializer, DocumentoSerializer
 
-# Create your views here.
+class ExpedienteViewSet(viewsets.ModelViewSet):
+    queryset = Expediente.objects.all()
+    serializer_class = ExpedienteSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['numero_expediente', 'parte', 'distrito_judicial']
+
+class DocumentoViewSet(viewsets.ModelViewSet):
+    queryset = Documento.objects.all()
+    serializer_class = DocumentoSerializer
