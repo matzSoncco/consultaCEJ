ef1701ac685c15a9bc9b32d3755c42896945d569
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2025-10-15 03:27:15 -0500

    Arregla import

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Expediente from '@/views/Expediente.vue'
 import ExpedienteDetalle from '@/views/ExpedienteDetalle.vue'
