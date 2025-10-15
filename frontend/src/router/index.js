8f7e43be55785dfaaec45a9f4c66e774753881eb
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2025-10-15 03:24:17 -0500

    Para manejar rutas ingreasadas manualmente

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,16 +4,14 @@ import Expediente from '@/views/Expediente.vue'
 import ExpedienteDetalle from '@/views/ExpedienteDetalle.vue'
 import SubirDocumentos from '@/views/SubirDocumentos.vue'
 
-const routes = [
-  { path: '/', name: 'home', component: Home },
-  { path: '/expediente/:id', name: 'expediente', component: Expediente, props: true },
-  { path: '/expediente/:id/detalle', name: 'expediente-detalle', component: ExpedienteDetalle, props: true },
-  { path: '/subir-documento', name: 'subir-documento', component: SubirDocumentos },
-]
-
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+  history: createWebHashHistory(),
+  routes: [
+    { path: '/', name: 'home', component: Home },
+    { path: '/expediente/:id', name: 'expediente', component: Expediente, props: true },
+    { path: '/expediente/:id/detalle', name: 'expediente-detalle', component: ExpedienteDetalle, props: true },
+    { path: '/subir-documento', name: 'subir-documento', component: SubirDocumentos },
+  ],
 })
 
 export default router
